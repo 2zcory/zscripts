8f4af9f672d220fadbc0a885f91701f7169fd407
TriNK「コリー」 <khanhtri009@gmail.com>
2024-09-24 12:00:12 +0700

    2024/09/24 11:58

---
+++ init.sh
@@ -0,0 +1,14 @@
+zsc=$HOME/.zscripts
+zwp=$HOME/wp
+
+if [ ! -d $zwp ]; then
+  echo 'Created workspace folder with path ' $zwp
+  mkdir $zwp
+fi
+
+if [ ! -z "$( ls -A $zwp)" ]; then
+  for d in $zwp/*/; do 
+    dname=$(basename $d)
+    declare zwp_$dname=$zwp/$dname
+  done
+fi
